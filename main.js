463f51529ca59773117e51ff2062dca4728a355a
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-11-11 01:37:38 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// let menu = document.getElementById("menu")
+// let links = document.getElementById("links")
+
+// menu.addEventListener("click", function () {
+//     links.classList.toggle("show")
+// })
+
+
+// let menu = document.getElementById("menu")
+// let links = document.getElementById("nav-links")
+// let nav = document.querySelector("nav")
+
+// menu.addEventListener("click", function () {
+//     links.classList.toggle("show")
+
+// })
+
+let div = document.getElementById("div")
+let b = document.getElementById("b")
+
+div.addEventListener("click", function () {
+    alert("Div was clicked")
+})
+
+document.body.addEventListener("click", function(){
+    alert("body was clicked")
+})
